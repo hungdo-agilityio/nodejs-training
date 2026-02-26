50c17220f8dcde26b9af756ea9d18ae847d22540
Hung Do <hung.do@asnet.com.vn>
2026-02-26 14:04:37 +0700

    feat: add metadata for staff dashboard

--- client/src/app/staff/layout.tsx
+++ client/src/app/staff/layout.tsx
@@ -1,4 +1,9 @@
 import { RoleGuard } from '@/components';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Salon Booking - Staff Dashboard',
+};
 
 export default function StaffLayout({
   children,
